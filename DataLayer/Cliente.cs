8045f6b82677f04f4d6cc2e8f5f8b98a8e8533f4
Denisexper <clasic.exper2023@gmail.com>
2025-02-20 08:37:30 -0600

    feat: data_client

--- DataLayer/Cliente.cs
+++ DataLayer/Cliente.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +11,49 @@ namespace DataLayer
 {
     public class Cliente
     {
-        public bool ValidarCliente(string nombre, string apellido)
+        public bool validarUsuario(string usuario, string contra)
         {
-            return true;
-            //cliente data here
+
+            string conexionString = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(conexionString))
+            {
+
+                con.Open();
+                using (SqlCommand consulta = new SqlCommand("SELECT * FROM usuarios WHERE usuario = @usuario AND contraseña = @contraseña", con))
+                {
+
+                    consulta.Parameters.AddWithValue("@usuario", usuario);
+                    consulta.Parameters.AddWithValue("@contraseña", contra);
+
+                    object result = consulta.ExecuteScalar();
+                    int count = (result != null) ? Convert.ToInt32(result) : 0;
+                    return count > 0;
+
+                }
+
+            }
+
         }
+
+        public DataTable ObtenerUsuarios()
+        {
+            string conexionString = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+            DataTable dt = new DataTable();
+            using (SqlConnection conn = new SqlConnection(conexionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT id, usuario FROM usuarios", conn))
+                {
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        da.Fill(dt);
+                    }
+                }
+            }
+            return dt;
+        }
+
     }
 }
+
